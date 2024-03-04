8382e299f2ccd2f58ebb4e4b094fa89301f24074
Jagriti <jagritii0705@gmail.com>
2024-03-04 13:12:02 +0530

    created signup api route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+mongoose.connect("mongodb+srv://jagriti:jagriti@mern-estate.uu6f755.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
+.then(console.log("connected to Db"))
+.catch((err)=>{
+    console.log(err);
+});
+
+app.use(express.json());
 
 app.listen("3000" ,()=>{
     console.log(`server running in port 3000`);
 });
+
+
+app.use("/api/user" , userRouter);
+app.use("/api/auth" , authRouter);
 
