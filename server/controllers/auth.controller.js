be0d2d5a9d504b669ea823a1453e38c686b0284d
Jagriti <jagritii0705@gmail.com>
2024-03-07 14:08:05 +0530

    create update user api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -39,7 +39,7 @@ export const google = async (req, res, next) => {
     try {
       const user = await User.findOne({ email: req.body.email });
       if (user) {
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: user._id }, 'uygdiuhoihjpjpojpojpojpojpojpojpjp7587575');
         const { password: pass, ...rest } = user._doc;
         res
           .cookie('access_token', token, { httpOnly: true })
@@ -59,7 +59,7 @@ export const google = async (req, res, next) => {
           avatar: req.body.photo,
         });
         await newUser.save();
-        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: newUser._id },'uygdiuhoihjpjpojpojpojpojpojpojpjp7587575' );
         const { password: pass, ...rest } = newUser._doc;
         res
           .cookie('access_token', token, { httpOnly: true })
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -14,6 +15,7 @@ mongoose.connect("mongodb+srv://jagriti:jagriti@mern-estate.uu6f755.mongodb.net/
 });
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen("3000" ,()=>{
     console.log(`server running in port 3000`);
