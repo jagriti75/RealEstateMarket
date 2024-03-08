96bb5236726fb0ee0e045dcf97de66ea3c2b6960
Jagriti <jagritii0705@gmail.com>
2024-03-08 14:34:47 +0530

    complete user update functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,12 +2,13 @@ import React, { useState, useRef, useEffect } from "react";
 import styles from "./Profile.module.css";
 import { getDownloadURL, getStorage, uploadBytesResumable, ref } from 'firebase/storage'
 import { app } from '../firebase';
-import { useSelector } from "react-redux";
+import { useSelector ,useDispatch } from "react-redux";
+import { updateUserStart ,updateUserSuccess ,updateUserFailure, signInFailure } from "../redux/user/userSlice";
 import edit from '../assets/edit.png'
 
 export const Profile = () => {
 
-    const { currentUser } = useSelector((state) => state.user);
+    const { currentUser , loading , error } = useSelector((state) => state.user);
     const [name, setName] = useState(false);
     const [email, setEmail] = useState(false);
     const [pass, setPass] = useState(false);
@@ -15,7 +16,10 @@ export const Profile = () => {
     const [errorUpload, setErrorUpload] = useState(false);
     const [uploadPerc, setUploadPerc] = useState(0);
     const [formData, setFormData] = useState({});
+    const [success , setUpdateSuccess] = useState(false);
     const fileRef = useRef(null);
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (file) {
@@ -46,8 +50,6 @@ export const Profile = () => {
         );
 
     }
-
-
     const handleNameEdit = () => {
         setName(true);
     }
@@ -60,11 +62,39 @@ export const Profile = () => {
         setPass(true);
     }
 
+    const handleChange = (e) => {
+        setFormData({...formData , [e.target.id] : e.target.value});
+        console.log(formData);
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          dispatch(updateUserStart());
+          const res = await fetch(`/api/user/update/${currentUser._id}`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+          const data = await res.json();
+          if (data.success === false) {
+            dispatch(updateUserFailure(data.message));
+            return;
+          }
+    
+          dispatch(updateUserSuccess(data));
+          setUpdateSuccess(true);
+        } catch (error) {
+          dispatch(updateUserFailure(error.message));
+        }
+      };
+    
     return (
         <div className={styles.container}>
             <h1>PROFILE</h1>
             <div >
-                <form className={styles.information}>
+                <form onSubmit={handleSubmit} className={styles.information} >
                     <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
                     <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} />
                     <p>
@@ -83,30 +113,35 @@ export const Profile = () => {
                     <div className={styles.info}>
                         <span>name :</span>
                         <span>
-                            {name ? <input type="text" id="username" /> : currentUser.username}
+                            {name ? <input type="text" id='username' onChange={handleChange} /> : currentUser.username}
                         </span>
-                        <span><button className={styles.edit} type='button' onClick={handleNameEdit}><img src={edit} /></button></span>
+                        <span><button className={styles.edit} type='button' onClick={handleNameEdit} ><img src={edit} /></button></span>
                     </div>
                     <div className={styles.info}><
                         span>email :</span>
                         <span>
-                            {email ? <input type="text" id="email" /> : currentUser.email}
+                            {email ? <input type="text" onChange={handleChange} id='email'/>  : currentUser.email}
                         </span>
-                        <span><button className={styles.edit} type='button' onClick={handleEmailEdit}><img src={edit} /></button></span>
+                        <span><button className={styles.edit} type='button' onClick={handleEmailEdit} ><img src={edit} /></button></span>
                     </div>
                     <div className={styles.info}><
                         span>password :</span>
                         <span>
-                            {pass ? <input type="text" id="password" /> : "xxxxx"}
+                            {pass ? <input type="password" id='password' onChange={handleChange} /> : currentUser.password}
                         </span>
-                        <span><button className={styles.edit} type='button' onClick={handlePassEdit}><img src={edit} /></button></span>
+                        <span><button className={styles.edit} type='button' onClick={handlePassEdit} ><img src={edit} /></button></span>
                     </div>
-                    <button className={styles.options}>Update</button>
+                    <button disabled={loading} type="submit" className={styles.options}>
+                        {loading ? "loading..." : "update"}
+                        </button>
                 </form>
                 <div className={styles.update}>
                     <a href="">Delete account</a>
                 </div>
-
+                <div>
+                <p className={styles.red}>{error? error : " "}</p>
+                <p className={styles.green}>{success? "updated successfully!" : " "}</p>
+                </div>
             </div>
         </div>
     );
