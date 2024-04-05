cfcdc577d4901870a5900d8dc1f63b59b76526cf
Jagriti <jagritii0705@gmail.com>
2024-04-05 15:55:32 +0530

    updated

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -6,6 +6,10 @@ import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
 
     const { username, email, password } = req.body;
+    const name = await User.findOne({username});
+    if(name){
+      next(errorHandler(400 ,"username already exists!"));
+    }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     try {
