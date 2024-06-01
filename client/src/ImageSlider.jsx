21dfa3836ceed91a1abef8bcdf6e2241e05d6b3e
Jagriti <jagritii0705@gmail.com>
2024-06-01 13:56:36 +0530

    responsive update

--- client/src/ImageSlider.jsx
+++ client/src/ImageSlider.jsx
@@ -10,8 +10,7 @@ const ImageSlider = ({ images }) => {
             <div className={styles.imageContainer} style={{ display: 'flex', flexWrap: 'wrap' }}>
                 {images.map((image, index) => (
                     <div key={index} style={{ margin: '10px', border: '1px solid #ccc' }}>
-                      <a href={image}><img src={image} title={`Image ${index+1}/${imagesLength}`} alt={`image ${index}`} style={{height:'400px' ,width:'900px'
-                    ,scrollSnapAlign:'start'}}  /></a>
+                      <a href={image}><img src={image} title={`Image ${index+1}/${imagesLength}`} alt={`image ${index}`} style={{height:'400px' ,width:'900px',scrollSnapAlign:'start'}}  /></a>
                     </div>
                 ))}
             </div>
